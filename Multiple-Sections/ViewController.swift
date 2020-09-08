803cf9e7cddf10857d29ed76e4af430ca77890d0
Juan C <juanceballos@pursuit.org>
2020-09-08 16:56:09 -0400

    multiple sections compositional layout, fc

---
+++ Multiple-Sections/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Multiple-Sections
+//
+//  Created by Juan Ceballos on 9/8/20.
+//  Copyright © 2020 Juan Ceballos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
